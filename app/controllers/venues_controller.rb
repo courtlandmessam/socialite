affe7bceff296dfbc1f8d45559c35f0ce444d3a4
Courtland Messam <messamcourtland@gmail.com>
2017-03-02 00:03:19 -0500

    add form to new venue view

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,4 +1,18 @@
 class VenuesController < ApplicationController
   def index
   end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    @venue = Venue.create(venue_params)
+    redirect_to venues_path
+  end
+  private
+
+def venue_params
+  params.require(:venue).permit(:image, :title, :location, :description)
+end
 end
