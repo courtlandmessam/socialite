a9630cf5fe0ecdb6a144ad17545a3ab535432599
Courtland Messam <messamcourtland@gmail.com>
2017-03-02 12:15:14 -0500

    Fix venues and events

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -5,17 +5,36 @@ class VenuesController < ApplicationController
     @venues = Venue.all
   end
 
+  def show
+    @venue = Venue.find(params[:id])
+  end
+
   def new
     @venue = Venue.new
+  end
+
+  def edit
   end
 
   def create
     @venue = Venue.create(venue_params)
     redirect_to venues_path
   end
-  private
 
-def venue_params
-  params.require(:venue).permit(:image, :title, :location, :description)
-end
+  def update
+    respond_to do |format|
+      if @venue.update(venue_params)
+        format.html { redirect_to @venue, notice: 'Venue was successfully updated.' }
+        format.json { render :show, status: :ok, location: @venue }
+      else
+        format.html { render :edit }
+        format.json { render json: @venue.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def venue_params
+      params.require(:venue).permit(:image, :title, :location, :description)
+    end
 end
