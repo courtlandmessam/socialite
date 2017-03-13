95eb794e3db2b7e19ff4b117d2cda1b98eb21a10
Courtland Messam <messamcourtland@gmail.com>
2017-03-13 03:12:53 -0400

    finish user and manager show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,42 @@ def show
 	@events = current_user.events
 	@deals = current_user.deals
 
+	@votes = Vote.all
+	@all_comments = Comment.all
+	@all_deals = Deal.all
+	@all_events = Event.all
+	@all_venues = Venue.all
+
+	@user_liked_comments_id = []	
+	@votes.each do |vote|
+		if vote.voteable_type == 'Comment' && vote.voter_id == current_user.id && vote.vote == true
+			@user_liked_comments_id << vote.voteable_id
+		end
+	end
+
+	@user_liked_deals_id = []
+	@votes.each do |vote|
+		if vote.voteable_type == 'Deal' && vote.voter_id == current_user.id && vote.vote == true
+			@user_liked_deals_id << vote.voteable_id
+		end
+	end
+
+
+	@user_liked_events_id = []
+	@votes.each do |vote|
+		if vote.voteable_type == 'Event' && vote.voter_id == current_user.id && vote.vote == true
+			@user_liked_events_id << vote.voteable_id
+		end
+	end
+
+	@user_liked_venues_id = []
+	@votes.each do |vote|
+		if vote.voteable_type == 'Venue' && vote.voter_id == current_user.id && vote.vote == true
+			@user_liked_venues_id << vote.voteable_id
+		end
+	end
+
+
 end
 
 end
