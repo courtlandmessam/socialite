55754221d2fa711400f4b383142593ad23f2db80
Courtland Messam <messamcourtland@gmail.com>
2017-03-13 13:50:45 -0400

    created claim venue page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :claim_venues
   resources :feedbacks
   resources :deals do
     member do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313073038) do
+ActiveRecord::Schema.define(version: 20170313174046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 20170313073038) do
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "claim_venues", force: :cascade do |t|
+    t.string   "name"
+    t.string   "venue"
+    t.string   "email"
+    t.string   "phone_number"
+    t.text     "other"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "comments", force: :cascade do |t|
